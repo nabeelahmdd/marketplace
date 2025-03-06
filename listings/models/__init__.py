7cda15381180968406528bdf5ac003e86b018a35
Nabeel Ahmed <nabeel.ahmed@indianic.com>
2025-03-06 20:50:39 +0530

    added new apps

--- listings/models/__init__.py
+++ listings/models/__init__.py
@@ -1,12 +1,12 @@
 from .category_models import Category
 from .tag_models import Tag
 from .listing_models import Listing, ListingImage
-from. user_intrection_models import (
-    Favorite,
-    Comment,
-    Rating,
-    Report,
-    SearchQuery,
-    SavedSearch,
-    RecommendedListing
-)
+# from. user_intrection_models import (
+#     Favorite,
+#     Comment,
+#     Rating,
+#     Report,
+#     SearchQuery,
+#     SavedSearch,
+#     RecommendedListing
+# )
